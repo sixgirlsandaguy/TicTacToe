4ad2814766c495fab9317e7d5271ce57a75864f1
Your Name <you@example.com>
2015-10-29 23:31:17 +0000

    Added setCell to Board class and unit test for it

--- src/main/java/ru/sixgirlsandaguy/ticTacToe/Board.java
+++ src/main/java/ru/sixgirlsandaguy/ticTacToe/Board.java
@@ -2,30 +2,61 @@ package ru.sixgirlsandaguy.ticTacToe;
 
 public class Board {
 
-	public static void main(String[] args) {
-				
-		Board b = new Board();
-			
-	} 
-
 	public static int dim = 3;
 	private	char[][] board;
 
 	public Board() 
 	{
 		board = new char [dim][dim];
-
+		
 		char temp = 49;
 
 		for (int i = 0; i < Board.dim; i++)
 		{
 			for (int j = 0; j < Board.dim; j++)
 			{
+				
 				board[i][j] = temp;
 				temp++;
 			}
 		}
 	}
+	
+	public void setCell(char player, int N)
+	{
+		int [] pos = cell_help(N);
+		
+		board[pos[0]][pos[1]] = player;
+	}
+	
+	public char getCell(int N)
+	{
+		int [] pos = cell_help(N);
+		
+		return board[pos[0]][pos[1]];
+	}
+	
+	private int[] cell_help(int N)
+	{
+		
+		int [] res = new int[2];
+		int row = N/3;
+		if(N%3 == 0){
+			row --;
+		}
+		int col = N % 3;
+		if(col == 0){
+			col = 3;
+		}
+		col--;
+		
+		res[0] = row;
+		res[1] = col;
+		
+		return res;
+		
+	}
+	
 	public void printBoard() 
 	{
 		for (int i = 0; i < Board.dim; i++)
@@ -37,23 +68,6 @@ public class Board {
 			System.out.println();
 		}
 	}
-	
-	public char getCell(int N)
-	{
-		int row = N/3;
-		if(N%3 == 0){
-			row --;
-		}
-		int col = N % 3;
-		if(col == 0){
-			col = 3;
-		}
-		col--;
-		
-		
-		return board[row][col];
-	}
-
 }
 
 
--- src/test/java/ru/sixgirlsandaguy/ticTacToe/BoardTest.java
+++ src/test/java/ru/sixgirlsandaguy/ticTacToe/BoardTest.java
@@ -18,4 +18,20 @@ public class BoardTest {
 		Board b = new Board();
 		assertEquals('1', b.getCell(1));
 	}
+
+	@Test 
+	public void testSetCell()
+	{
+		Board b = new Board();
+		b.setCell('X', 6);
+		assertEquals('X', b.getCell(6));
+	}
+
+	@Test 
+	public void testSetCellForO()
+	{
+		Board b = new Board();
+		b.setCell('O', 6);
+		assertEquals('O', b.getCell(6));
+	}
 }
