27cd74a1a98e86d90a6f31f6e865560cb93f424a
Your Name <you@example.com>
2015-10-29 18:27:54 +0000

    Board class made with constructur and test for Board

--- src/main/java/ru/sixgirlsandaguy/ticTacToe/Board.java
+++ src/main/java/ru/sixgirlsandaguy/ticTacToe/Board.java
@@ -1,3 +1,32 @@
-//package ru.sixgirlsandaguy.ticTacToe;
+package ru.sixgirlsandaguy.ticTacToe;
+
+public class Board {
+
+	public static void main(String[] args) {
+				
+		Board b = new Board();
+			
+	} 
+
+	public static int dim = 3;
+	private	char[][] board;
+
+	public Board() 
+	{
+		board = new char [dim][dim];
+
+		char temp = 49;
+
+		for (int i = 0; i < Board.dim; i++)
+		{
+			for (int j = 0; j < Board.dim; j++)
+			{
+				board[i][j] = temp;
+				temp++;
+			}
+		}
+	}
+
+}
 
 
--- src/test/java/ru/sixgirlsandaguy/ticTacToe/BoardTest.java
+++ src/test/java/ru/sixgirlsandaguy/ticTacToe/BoardTest.java
@@ -1,2 +1,15 @@
-//package ru.sixgirlsandaguy.ticTacToe;
+package ru.sixgirlsandaguy.ticTacToe;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+
+public class BoardTest {
+
+	public static void main(String args[]) 
+	{
+      org.junit.runner.JUnitCore.main("ru.sixgirlsandaguy.ticTacToe");
+    }
+
+}
