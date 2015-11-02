fa29bc2c06ff087e26f67ca7687d936d6d0fa1bb
Huldar Bjarmi Halldórsson <huldar14@i-7f3b5dd9.csd07cloud.internal>
2015-11-02 10:34:16 +0000

    script changes

--- src/main/resources/public/TicTacToeScript.js
+++ src/main/resources/public/TicTacToeScript.js
@@ -5,13 +5,14 @@ $(function(){
 	var cell = $(this).attr('id'); 
 	if($(this).html()==""){
 	    $.ajax({
-		url: "/cell"
-		type: "post"
+		url: "/cell",
+		type: "post",
 		data: cell
 	   
 	   }).done(function(result)){
 	       $("#" + cell).html(result);
-	}    
+	});
+	};
 	event.preventDefault();
 	
     });
