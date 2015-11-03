a858ccdf4d5c875e452af0b4b0a932e13f4b3ebd
Agnes <agnesjoha@gmail.com>
2015-11-03 02:49:46 +0000

    selenium works

---
+++ src/selenium/java/ru/sixgirlsandaguy/ticTacToe/SeleniumTest.java
@@ -0,0 +1,192 @@
+package ru.sixgirlsandaguy.ticTacToe;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://sixgirlsandaguy.herokuapp.com";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testEven() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.id("1")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("X".equals(driver.findElement(By.id("1")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("X", driver.findElement(By.id("1")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("6")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("O".equals(driver.findElement(By.id("6")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("O", driver.findElement(By.id("6")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("5")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("X".equals(driver.findElement(By.id("5")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("X", driver.findElement(By.id("5")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("2")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("O".equals(driver.findElement(By.id("2")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("O", driver.findElement(By.id("2")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("8")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("X".equals(driver.findElement(By.id("8")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("X", driver.findElement(By.id("8")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("9")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("O".equals(driver.findElement(By.id("9")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("O", driver.findElement(By.id("9")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("4")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("X".equals(driver.findElement(By.id("4")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("X", driver.findElement(By.id("4")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("7")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("O".equals(driver.findElement(By.id("7")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("O", driver.findElement(By.id("7")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("3")).click();
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("X".equals(driver.findElement(By.id("3")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("X", driver.findElement(By.id("3")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if ("Draw!".equals(driver.findElement(By.id("winner")).getText())) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+
+    try {
+      assertEquals("Draw!", driver.findElement(By.id("winner")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    driver.findElement(By.id("restart")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
+
